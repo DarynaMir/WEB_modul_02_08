eee4771ab671ecd4e05e7d833c08201ac50fe1ca
Daryna Mirzaholi <dmirzagholi@gmail.com>
2024-03-03 01:10:57 +0000

    first_commit

---
+++ models.py
@@ -0,0 +1,19 @@
+import mongoengine
+
+from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE
+
+connect(db="homework", host="mongodb://localhost:27017")
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField(max_length=200)
+    description = StringField()
+    meta = {"collection": "authors"}
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=15))
+    quote = StringField()
+    meta = {"collection": "quotes"}
+
